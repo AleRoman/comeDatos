5873acedaa957f7e6c47798d3eef68833c928d9e
JUVALE INC <juvale0911@gmail.com>
2019-09-23 01:46:09 -0500

    Agregando la petición para obtener los tramites

---
+++ src/app/models/tramite_model.ts
@@ -0,0 +1,12 @@
+export interface Tramite {
+    dependencia:    string;
+    direccion:      string;
+    nombre:         string;
+    telefono:       string;
+    hora_atencion:  string;
+    referencia:     string;
+    costo:          string;
+    tipo_pago:      string;
+    sitio_atencion: string;
+    requisito:      string[];
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Tramite } from '../models/tramite_model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  url = 'https://cors-anywhere.herokuapp.com/tramites.gestionpublica.qroo.gob.mx/sistema/wstramites.php';
+  tramites: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(res => {
+      console.log(res);
+      this.tramites = res;
+      console.log(this.tramites);
+    });
+  }
 
 }
